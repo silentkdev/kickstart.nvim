1b83baf2c027fb95a312e701bb477e002091e7a9
Andrew Knox <179271300+silentkdev@users.noreply.github.com>
2024-11-09 14:47:04 -0800

    adding csharp_ls

--- init.lua
+++ init.lua
@@ -615,6 +615,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         bashls = {},
+        csharp_ls = {},
         diagnosticls = {},
         docker_compose_language_service = {},
         dockerls = {},
